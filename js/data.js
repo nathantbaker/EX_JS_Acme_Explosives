129adcb7ecfcdc9f10338f2737e2cdf187d69533
Nathan T. Baker <nathantbaker@gmail.com>
2016-11-09 22:41:18 -0600

    removed console logs, es6'ed it, added logo

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-var Acme = (function (Acme) {
+var Acme = ( Acme => {
 
 let allData = {},
   dataFunctions = [],
@@ -9,18 +9,17 @@ let allData = {},
   };
 
 //getter for data we're collecting
-Acme.getData = function () {
+Acme.getData = () => {
   return allData;
 };
 
 // Looks at apiCalls object and creates promises for each call
-Acme.callAPIs = function () {
+Acme.callAPIs = () => {
 
   for (var prop in apiCalls) {
-    console.log("apiCalls." + prop + " = " + apiCalls[prop]);
 
-    dataFunctions[prop] = (function (prop) { // iife allows prop to not have closure in loop
-      return function () {
+    dataFunctions[prop] = ( prop => { // iife allows prop to not have closure in loop
+      return () => {
         return new Promise((resolve, reject) => {
           $.ajax({
             url: apiCalls[prop]
@@ -34,7 +33,7 @@ Acme.callAPIs = function () {
     })(prop);
   }
 
-  // Push data to get
+  // Push data to parser
   Promise.all([dataFunctions.categories(), dataFunctions.types(), dataFunctions.products()])
   .then( arrayOfData => {
     Acme.parseData(arrayOfData);
@@ -42,7 +41,8 @@ Acme.callAPIs = function () {
 
 };
 
-Acme.parseData = function (arrayOfData) {
+// Parse and push data to page
+Acme.parseData = arrayOfData => {
 
     allData = {
       "categories": arrayOfData[0].categories,
@@ -52,11 +52,10 @@ Acme.parseData = function (arrayOfData) {
 
     // After Data is pushed, setup listeners on page
     Acme.listenToPage();
-
 };
 
 return Acme;
 
 })(Acme || {});
 
-Acme.callAPIs();
+Acme.callAPIs(); // get the data
